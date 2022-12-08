a0dbac9fc98c181c8704fb871b8cb71b361b2d69
Axel Enghamre <axel@enghamre.se>
2022-12-08 14:49:11 +0100

    added main script with some feelings

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+// main js
+// run index 0
+const select = document.querySelector('select');
+const main = document.querySelector('main');
+
+// feeling object template
+// {
+//     feeling: '',
+//     color: '',
+// },
+
+const feelings = [
+  {
+    feeling: 'nothing',
+    color: 'transparent',
+  },
+  {
+    feeling: 'angry',
+    color: 'red',
+  },
+  {
+    feeling: 'sad',
+    color: 'black',
+  },
+  {
+    feeling: 'happy',
+    color: 'green',
+  },
+  {
+    feeling: 'enjoyment',
+    color: 'yellow',
+  },
+  {
+    feeling: 'horrified',
+    color: 'brown',
+  },
+  {
+    feeling: 'challenged',
+    color: 'linear-gradient(to right, red , yellow)',
+  },
+];
